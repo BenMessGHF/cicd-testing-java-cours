2eabcb01904552e554586af38a20919bc43f34af
29010-55-12 <29010-55-12@exchange.ad.afpanet>
2024-10-03 16:27:59 +0200

    remove firefox test

--- src/test/java/tech/zerofiltre/testing/calcul/e2e/MultiplicationJourneyE2ETest.java
+++ src/test/java/tech/zerofiltre/testing/calcul/e2e/MultiplicationJourneyE2ETest.java
@@ -20,54 +20,54 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MultiplicationJourneyE2ETest {
-
-  @LocalServerPort
-  private int port;
-
-  private WebDriver webDriver;
-  private String baseUrl;
-
-  @BeforeAll
-  static void setUpFireFoxDriver() {
-    WebDriverManager.firefoxdriver().setup();
-  }
-
-  @BeforeEach
-  void setUpWebDriver() {
-    webDriver = new FirefoxDriver();
-    baseUrl = "http://localhost:" + port + "/calculator";
-
-  }
-
-  @AfterEach
-  void quitWebDriver() {
-    if (webDriver != null) {
-      webDriver.quit();
-    }
-  }
-
-  @Test
-  void multiplyTwoBySixteenMustReturn32(){
-
-    //GIVEN
-    webDriver.get(baseUrl);
-    WebElement leftField = webDriver.findElement(By.id("left"));
-    WebElement typeDropDown = webDriver.findElement(By.id("type"));
-    WebElement rightField = webDriver.findElement(By.id("right"));
-    WebElement submitButton = webDriver.findElement(By.id("submit"));
-
-    //WHEN
-    leftField.sendKeys("2");
-    typeDropDown.sendKeys("x");
-    rightField.sendKeys("16");
-    submitButton.click();
-
-    //THEN
-    WebDriverWait waiter = new WebDriverWait(webDriver,5);
-    WebElement solutionElement = waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("solution")));
-    String solution = solutionElement.getText();
-    assertThat(solution).isEqualTo("32");
-  }
+//
+//  @LocalServerPort
+//  private int port;
+//
+//  private WebDriver webDriver;
+//  private String baseUrl;
+//
+//  @BeforeAll
+//  static void setUpFireFoxDriver() {
+//    WebDriverManager.firefoxdriver().setup();
+//  }
+//
+//  @BeforeEach
+//  void setUpWebDriver() {
+//    webDriver = new FirefoxDriver();
+//    baseUrl = "http://localhost:" + port + "/calculator";
+//
+//  }
+//
+//  @AfterEach
+//  void quitWebDriver() {
+//    if (webDriver != null) {
+//      webDriver.quit();
+//    }
+//  }
+//
+//  @Test
+//  void multiplyTwoBySixteenMustReturn32(){
+//
+//    //GIVEN
+//    webDriver.get(baseUrl);
+//    WebElement leftField = webDriver.findElement(By.id("left"));
+//    WebElement typeDropDown = webDriver.findElement(By.id("type"));
+//    WebElement rightField = webDriver.findElement(By.id("right"));
+//    WebElement submitButton = webDriver.findElement(By.id("submit"));
+//
+//    //WHEN
+//    leftField.sendKeys("2");
+//    typeDropDown.sendKeys("x");
+//    rightField.sendKeys("16");
+//    submitButton.click();
+//
+//    //THEN
+//    WebDriverWait waiter = new WebDriverWait(webDriver,5);
+//    WebElement solutionElement = waiter.until(ExpectedConditions.presenceOfElementLocated(By.id("solution")));
+//    String solution = solutionElement.getText();
+//    assertThat(solution).isEqualTo("32");
+//  }
 
 
 }
